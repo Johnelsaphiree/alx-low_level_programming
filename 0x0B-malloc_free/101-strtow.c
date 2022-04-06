32eeeb67735626f2dceb62f1d35a44c6c7d62df9
dev_Johnel <69057979+Johnelsaphiree@users.noreply.github.com>
2022-04-06 16:03:10 +0100

    done

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,85 +1,69 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
 
-/**
- * _wcount - counts number of words
- * @sw: string
- *
- * Return: int
- */
+int w_count(char *str, int size);
+int _strlen_recursion(char *s);
 
-int _wcount(char *sw)
-{
-	int l, wc;
 
-	l = 0, wc = 0;
-	if (*(sw + l) == ' ')
-		l++;
-	while (*(sw + l))
-	{
-		if (*(sw + l) == ' ' && *(sw + l - 1) != ' ')
-			wc++;
-		if (*(sw + l) != ' '  && *(sw + l + 1) == 0)
-			wc++;
-		l++;
-	}
-	return (wc);
-}
 /**
- * _trspace - Moves adress to remove trailig whitespaces
- * @st: string
- *
- * Return: Pointer
- */
-char *_trspace(char *st)
-{
-	while (*st == ' ')
-		st++;
-	return (st);
-}
-/**
- * strtow - splits a string into words
- * @str: string
- *
- * Return: Double Pointer
+ * **strtow - Splits a string in two words
+ * @str: String
+ * Return: Pointer to an array of strings
  */
 char **strtow(char *str)
 {
-	char **s, *ts;
-	int l, l2, wc, i, j, fr, k;
+	char *strcp, **strcon;
+	int len = 0, noWords;
 
-	if (str == NULL || *str == 0)
-		return (0);
-	fr = 0;
-	wc = _wcount(str);
-	if (wc == 0)
-		return (0);
-	s = malloc((wc + 1) * sizeof(char *));
-	if (s == 0)
-		return (0);
-	ts = _trspace(str);
-	for (i = 0; i < wc; i++)
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+
+	strcp = str;
+	len = _strlen_recursion(strcp);
+	noWords = w_count(str, len);
+
+	if (noWords < 1)
+		return (NULL);
+
+	strcon = malloc(noWords + 1 * sizeof(char *));
+
+	strcon[0] = malloc(sizeof(char) * 1 + 1);
+
+	return (strcon);
+}
+
+/**
+ * w_count - Count the total words in a string
+ * @str: String
+ * @size: length of the string
+ * Return: Count of words Integer
+ */
+int w_count(char *str, int size)
+{
+	int i, count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		l = 0;
-		while (*(ts + l) != ' ' && *(ts + l) != 0)
-			l++;
-		s[i] = malloc((l + 1) * sizeof(char));
-		if (s[i] == 0)
+		if (str[i] != ' ')
 		{
-			fr = 1;
-			break;
+			while (i < size && str[i] != ' ')
+				i++;
+			count++;
 		}
-		for (j = 0, l2 = 0; l2 < l; l2++, j++)
-			s[i][j] = *(ts + l2);
-		s[i][j] = '\0';
-		ts = _trspace(ts + l);
 	}
-	s[i] = NULL;
-	if (fr == 1)
-	{
-		for (k = 0; k <= i; k++)
-			free(s[k]);
-		free(s);
-	}
-	return (s);
+
+	return (count);
+}
+
+
+/**
+ * _strlen_recursion - Length of a string
+ * @s: char pointer
+ * Return: Integer variable
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(++s));
+	return (0);
+}
