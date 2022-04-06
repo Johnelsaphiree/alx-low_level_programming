2c53e20eca431abc8dcc9e07305d6cf8f401187f
dev_Johnel <69057979+Johnelsaphiree@users.noreply.github.com>
2022-04-06 16:00:51 +0100

    updated changes

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * _wcount - counts number of words
  * @sw: string
  *
  * Return: int
  */
+
 int _wcount(char *sw)
 {
 	int l, wc;
